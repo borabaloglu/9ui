a92a5eff28d43686ebea209b1b9eb0fb4f687675
Bora Baloglu <baloglub@gmail.com>
2025-05-28 15:50:25 +0100

    feat: tailwind v4 and react 19 version and config updates

--- apps/v2/eslint.config.mjs
+++ apps/v2/eslint.config.mjs
@@ -9,26 +9,15 @@ const config = [
 	...compat.config({
 		$schema: "https://json.schemastore.org/eslintrc",
 		root: true,
-		extends: [
-			"next/core-web-vitals",
-			"next/typescript",
-			"prettier",
-			"plugin:tailwindcss/recommended",
-		],
-		plugins: ["tailwindcss", "@typescript-eslint"],
+		extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+		plugins: ["@typescript-eslint"],
 		ignorePatterns: ["node_modules/", ".next/", "dist/", "build/"],
 		rules: {
 			"@next/next/no-html-link-for-pages": "off",
 			"react/jsx-key": "off",
-			"tailwindcss/no-custom-classname": "off",
 			"@next/next/no-img-element": "off",
 			"react-hooks/exhaustive-deps": "off",
-		},
-		settings: {
-			tailwindcss: {
-				callees: ["cn"],
-				config: "./tailwind.config.js",
-			},
+			"react/display-name": "off",
 		},
 	}),
 	{
