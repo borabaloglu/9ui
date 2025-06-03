5bc0ce96ac3773ae12b8ff0fdff41b10e94218ab
Bora Baloglu <baloglub@gmail.com>
2025-06-03 17:08:50 +0100

    feat: textarea v2

--- apps/v2/src/components/ui/textarea.tsx
+++ apps/v2/src/components/ui/textarea.tsx
@@ -2,19 +2,17 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Textarea = React.forwardRef<
-	HTMLTextAreaElement,
-	React.TextareaHTMLAttributes<HTMLTextAreaElement>
->(({ className, ...props }, ref) => (
-	<textarea
-		ref={ref}
-		className={cn(
-			"flex min-h-20 w-full rounded-md border bg-input p-4 text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 aria-[invalid=true]:border-destructive aria-[invalid=true]:text-destructive aria-[invalid=true]:placeholder:text-destructive aria-[invalid=true]:focus:ring-destructive md:text-sm",
-			className
-		)}
-		{...props}
-	/>
-))
-Textarea.displayName = "Textarea"
+function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
+	return (
+		<textarea
+			data-slot="textarea"
+			className={cn(
+				"placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/50 aria-invalid:border-destructive bg-input flex field-sizing-content min-h-16 w-full rounded-md border px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+				className
+			)}
+			{...props}
+		/>
+	)
+}
 
 export { Textarea }
