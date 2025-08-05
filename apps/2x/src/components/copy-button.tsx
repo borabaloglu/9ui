6473feb3bbf066bcb06efa6f59154631286ff0a2
Bora Baloglu <baloglub@gmail.com>
2025-08-05 23:07:18 +0100

    feat: add op events

--- apps/2x/src/components/copy-button.tsx
+++ apps/2x/src/components/copy-button.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from "react"
+import { useOpenPanel } from "@openpanel/nextjs"
 import { CheckIcon, CopyIcon } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
@@ -10,16 +11,27 @@ import { cn } from "@/lib/utils"
 interface CopyButtonProps
 	extends React.ComponentPropsWithoutRef<typeof Button> {
 	content: string
+	label?: string
+	eventType?: "demo" | "component" | "command" | "other"
 }
 
 export const CopyButton = ({
 	content,
+	eventType,
+	label,
 	className,
 	...props
 }: CopyButtonProps) => {
+	const op = useOpenPanel()
+
 	const [copied, setCopied] = React.useState(false)
 
 	const onCopy = async () => {
+		op.track("copy", {
+			type: eventType || "other",
+			content: eventType === "command" ? content : undefined,
+			label,
+		})
 		await navigator.clipboard.writeText(content!)
 		setCopied(true)
 		setTimeout(() => setCopied(false), 2000)
